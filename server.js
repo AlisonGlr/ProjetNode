7afaa6cf88e7cc033c1e0f55dd537e0447fd385d
ALISON GUILLORE (Etudiant) <guilla31@ur.univ-rouen.fr>
2024-02-23 11:27:38 +0100

    ajout bootstrap

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ let express = require ('express');
 let app = express();
 let port = 3000;
 
+app.use(express.static(__dirname + '/www'));
+app.use('/js', express.static(__dirname + '/node_modues/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modues/bootstrap/dist/css'));
+
 app.listen(port, () => {
     console.log('le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
 app.get('/', (req, res, next) => {
-    res.send('Bonjour !');
-})
+    res.sendFile('/www/index.html');
+});
